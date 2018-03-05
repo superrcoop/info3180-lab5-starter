aba563b245a0464f43640bd46069478022b17cae
Jordan Cooper <superrcoop@Jordans-MacBook-Pro.local>
2018-03-04 21:51:43 -0500

    add secure page and implement session(user)login_is_required

--- app/views.py
+++ app/views.py
@@ -27,26 +27,38 @@ def about():
     """Render the website's about page."""
     return render_template('about.html')
 
+@app.route('/secure-page')
+@login_required
+def secure_page():
+    """Render a secure page on our website that only logged in users can access."""
+    return render_template('secure_page.html')
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    if current_user.is_authenticated:
+        # if user is already logged in, just redirect them to our secure page
+        # or some other page like a dashboard
+        return redirect(url_for('secure_page'))
+    
     form = LoginForm()
-    if request.method == "POST":
-        # change this to actually validate the entire form submission
-        # and not just one field
-        if form.username.data:
-            # Get the username and password values from the form.
-
-            # using your model, query database for a user based on the username
-            # and password submitted
-            # store the result of that query to a `user` variable so it can be
-            # passed to the login_user() method.
+    if request.method == "POST" and form.validate_on_submit():
+        
+        # # Get the username and password values from the form.
+        username = form.username.data
+        password = form.password.data
+        user = UserProfile.query.filter_by(username=username).first()
+        if user is not None:
+            
 
             # get user id, load into session
             login_user(user)
+            flash('Logged in successfully.', 'success')
 
-            # remember to flash a message to the user
-            return redirect(url_for("home"))  # they should be redirected to a secure-page route instead
+            next_page = request.args.get('next')
+            return redirect(next_page or url_for('home'))
+        else:
+            flash('Username or Password is incorrect.', 'danger')
+
     return render_template("login.html", form=form)
 
 
