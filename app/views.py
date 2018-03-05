f8089aa9d49e199307405364d4cf9c0a97b868e1
Jordan Cooper <superrcoop@Jordans-MacBook-Pro.local>
2018-03-04 21:53:01 -0500

    logout user

--- app/views.py
+++ app/views.py
@@ -61,6 +61,15 @@ def login():
 
     return render_template("login.html", form=form)
 
+@app.route("/logout")
+@login_required
+def logout():
+    # Logout the user and end the session
+    logout_user()
+    flash('You have been logged out.', 'danger')
+    return redirect(url_for('home'))
+
+
 
 # user_loader callback. This callback is used to reload the user object from
 # the user ID stored in the session
